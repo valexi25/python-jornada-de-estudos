d5bb2d535397e4597211bc3a140165a9c84e1218
Alexi Villarroel <villarroelalexi63@gmail.com>
2025-07-25 10:06:03 -0400

    Exercicio de Maioria de idade

---
+++ 1_Exercicios_Basicos/04_Logica_e_Condicionais/verificador_maioridade.py.py
@@ -0,0 +1,15 @@
+print("****  Verificador de idade ****")
+"""
+Objetivo: Crie um programa que verifique se uma pessoa é maior de idade. no brasil, a maior de idade é alcançada aos 18 anos.
+instruções:
+Crie uma variével camada idade e atribua um valor numérico a ela(exemp,25)
+Use uma estrutura if else para verificar se o valor da idade é maior ou igual a 18.
+Se for maior ou igual, o programa deve imprimir a mensagem: Maior de idade.
+Caso contrário (se for menor), o programa deve imprimir: Menor de idade.
+"""
+idade = 32
+
+if idade >=18:
+    print(f"Maior de idade tem {idade}")
+else:
+    print(f"Menor de idade: {idade}")
