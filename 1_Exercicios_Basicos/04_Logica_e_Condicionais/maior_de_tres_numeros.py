7fb5699e97602840ed1fb677050e616beff46e87
Alexi Villarroel <villarroelalexi63@gmail.com>
2025-07-28 08:04:30 -0400

    Adiciona exercicio mair de tres números

---
+++ 1_Exercicios_Basicos/04_Logica_e_Condicionais/maior_de_tres_numeros.py
@@ -0,0 +1,11 @@
+print("*** Encontrar Numero Maior ****")
+nun1 = 5
+nun2 =344
+nun3 =938
+
+if (nun1 >= nun2) and(nun1 >= nun3):
+    print(f"Dos números: {nun1},{nun2} e {nun3} o numero maior es {nun1} ")
+elif (nun2 >= nun1) and (nun2 >= nun3):
+    print(f"Dos números: {nun1},{nun2} e {nun3} o numero maior es {nun2} ")
+else:
+    print(f"Dos números: {nun1},{nun2} e {nun3} o numero maior es {nun3} ")
