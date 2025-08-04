563ff1e847f2bffccf87c81f2b3ba5ad6d8296e2
Alexi Villarroel <villarroelalexi63@gmail.com>
2025-08-04 09:36:53 -0400

    Adiciona ejercicio FOR

---
+++ 4_Curso_Python_Espanol/01_conceptos_basicos/05_loops_e_Repeticao/ciclo_for.py
@@ -0,0 +1,13 @@
+print('*** Ciclo for ***')
+print("Recoremos los caracteres de una cadena")
+cadena = 'Hola Mundo'
+
+for letra in cadena:
+    print(letra, end=' ')
+
+print('\n')
+print('Recoremos la lista de frutas')
+frutas = ['Platano', 'Fresa','Mango']
+
+for fruta in frutas:
+    print(fruta,end=' ')
