046c7b06bb6e38371bf4aada7efa1a70f4be12e2
Alexi Villarroel <villarroelalexi63@gmail.com>
2025-08-04 09:42:09 -0400

    Adiciona ejercicio de RANGE

---
+++ 4_Curso_Python_Espanol/01_conceptos_basicos/05_loops_e_Repeticao/funcion_range.py
@@ -0,0 +1,15 @@
+print('**** Funcion Range ****')
+
+
+print("Secuencia de 0 al 4: ")
+
+for i in range(5):
+    print(i, end=" ")
+
+print('\n\nSecuencia del 10 al 20: ')
+for i in range(10,20 + 1):
+    print(i, end=" ")
+
+print('\n\nSecuencia del 20 al 30 de 2 en 2: ')
+for i in range(20,30 + 1,2):
+    print(i, end= " ")
