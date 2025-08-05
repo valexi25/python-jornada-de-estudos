621b7274a97954e6058b776b03f00aaf942eda9e
Alexi Villarroel <villarroelalexi63@gmail.com>
2025-08-05 08:38:07 -0400

    Adiciona ejercicio validacion de longitud de string

---
+++ 4_Curso_Python_Espanol/01_conceptos_basicos/02_manipulacion_de_strings/validacion_longitud_string.py
@@ -0,0 +1,9 @@
+print('*** Validacion de Formulario **')
+
+nome_usuario = None
+
+while not nome_usuario:
+    nome_usuario = input('Ingresa tu nombre de Usuario: ')
+
+
+print(f'Nombre de Usuario valido {nome_usuario}')
