ec73b9c6c3e2ed59933e3560874d36572acdbae6
Alexi Villarroel <villarroelalexi63@gmail.com>
2025-08-08 09:44:53 -0400

    Adiciona el ejercicio deiterar listas

---
+++ 4_Curso_Python_Espanol/01_conceptos_basicos/05_loops_e_Repeticao/iterar_listas.py
@@ -0,0 +1,11 @@
+print('**** Iterar Listas ****')
+
+nombres = ['Karla','Juan','Laura']
+
+for nombre in nombres:
+    print(nombre)
+
+lista_heterogenea = [100,True,'Ivonne']
+print()
+for lista in lista_heterogenea:
+    print(lista)
