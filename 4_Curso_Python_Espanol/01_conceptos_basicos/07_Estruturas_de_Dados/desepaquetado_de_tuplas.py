f10a1ba39df2350beee1ca927817b64f7f425478
Alexi Villarroel <villarroelalexi63@gmail.com>
2025-08-11 17:47:47 -0400

    Adiciona ejercicio desempaquetando tuplas

---
+++ 4_Curso_Python_Espanol/01_conceptos_basicos/07_Estruturas_de_Dados/desepaquetado_de_tuplas.py
@@ -0,0 +1,13 @@
+print('*** Desempaquetado de Tuplas ***')
+
+producto = ('POO1','Camisa','20.00')
+
+#Desenpaquetado 
+id,descripcion,precio = producto
+
+#Iprimir los valores
+print(f'Tupla completa: {producto}')
+
+#Valores independientes ye desempaquetados
+
+print(f'Producto: id = {id}, descripcion = {descripcion}, precio = {precio}')
