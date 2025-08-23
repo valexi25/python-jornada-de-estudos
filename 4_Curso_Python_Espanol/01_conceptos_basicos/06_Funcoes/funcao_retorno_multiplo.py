61a3b4de95f7f70126c26ed8f837ec80c583d01e
Alexi Villarroel <villarroelalexi63@gmail.com>
2025-08-23 11:22:50 -0400

    Adiciona ejercicio ce funcion retorno multiplo

---
+++ 4_Curso_Python_Espanol/01_conceptos_basicos/06_Funcoes/funcao_retorno_multiplo.py
@@ -0,0 +1,9 @@
+print('**** Funciones Python *****')
+
+#Definir una funcion para mandar a saludar
+
+def saludar(mesaje): #firma del metodo
+    print(f'mensaje resibido: {mesaje}')
+
+#Programa principal,llamamos a la funcion
+saludar('Hola a todos')
