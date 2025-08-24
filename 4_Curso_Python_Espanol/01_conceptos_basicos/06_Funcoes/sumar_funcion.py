bbb7756678ba5d5f4d4e5b3d8b8f8383d5247bfe
Alexi Villarroel <villarroelalexi63@gmail.com>
2025-08-24 07:15:45 -0400

    Adiciona ejercicio sumar funcion

---
+++ 4_Curso_Python_Espanol/01_conceptos_basicos/06_Funcoes/sumar_funcion.py
@@ -0,0 +1,6 @@
+#import modulo_funcion_sumar
+from modulo_funcion_sumar import sumar
+# Llamamos a la funcion
+resultado_funcion = sumar(8,5)
+print(f'Resultado función sumar: {resultado_funcion}')
+
